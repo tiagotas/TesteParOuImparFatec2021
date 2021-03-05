c867ca925732b2e127ea9f81f0bb2076b58d9ab2
TIAGO SILVA <frost.tiago@gmail.com>
2021-03-04 22:14:10 -0300

    Adicionar arquivos de projeto.

---
+++ TesteParOuImpar/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TesteParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero_qualquer;
+
+            Console.WriteLine("Digite um número inteiro qualquer: ");
+            numero_qualquer = Convert.ToInt32(Console.ReadLine());
+
+            double resto = numero_qualquer % 2;
+
+            //Console.WriteLine("O resto é {0} ", resto);
+
+            if(resto == 0)
+            {
+                Console.WriteLine("O número digitado é PAR");
+
+            } else
+            {
+                Console.WriteLine("O número digitado é ÍMPAR");
+            }
+
+
+            /**
+             * Segunda Maneira.
+             */
+
+
+            Console.WriteLine("Digite um número inteiro qualquer: ");
+            numero_qualquer = Convert.ToInt32(Console.ReadLine());
+
+
+            if ( (numero_qualquer % 2) == 0 )
+            {
+                Console.WriteLine("O número digitado é PAR");
+            }
+            else
+            {
+                Console.WriteLine("O número digitado é ÍMPAR");
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
